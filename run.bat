60d80753c9ae0091a7b9393d9db19fcf24e66832
yaunsine <1580909148@qq.com>
2023-04-18 12:55:04 +0800

    feat:添加bat脚本，适配windows

---
+++ run.bat
@@ -0,0 +1,32 @@
+@echo off
+set download_category="true"
+set category_url="https://blog.csdn.net/weixin_43792401/category_12292383.html"
+set article_url="https://blog.csdn.net/weixin_43792401/article/details/130065744"
+set start_page=1
+set page_num=100
+set markdown_dir=markdown
+set pdf_dir=pdf
+
+if %download_category% == "true" (
+    echo "download a category"
+    python -u main.py ^
+        --category_url %category_url% ^
+        --start_page %start_page% ^
+        --page_num %page_num% ^
+        --markdown_dir %markdown_dir% ^
+        --pdf_dir %pdf_dir% ^
+        --combine_together ^
+        --to_pdf 
+        @REM --with_title ^
+        @REM --rm_cache ^
+) else (
+    echo "download an article"
+    python -u main.py ^
+        --article_url %article_url% ^
+        --markdown_dir %markdown_dir% ^
+        --pdf_dir %pdf_dir% ^
+        --to_pdf 
+        @REM --with_title ^
+        @REM --rm_cache ^
+        @REM --combine_together ^
+)
